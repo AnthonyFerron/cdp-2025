b46bdb673605d3e65db43b3b2b5a9a3bdaae4480
AnthonyFerron <anthony.ferron74@gmail.com>
2025-11-03 14:29:39 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+        <h1 className="text-5xl font-bold text-zinc-900 sm:text-6xl">
+          Bienvenu pour le Cercle de projet 2025 !
+        </h1>
+      </main>
+    </div>
+  );
+}
