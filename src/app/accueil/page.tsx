2112142a6076022e97abc0f8fec3311ad88c8f3f
EGOUCHOLA <egoucholamalick229@gmail.com>
2025-11-05 16:17:04 +0100

    Page accueil proto OK

--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -16,25 +16,43 @@ export default function SignInPage() {
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-1 px-4">
-        <div>
-          <img className="" src="logo.png" alt="" />
+      <div className="grid grid-cols-2 gap-24 px-16">
+        <div className="p-8">
+          <img className="mx-auto w-1/4" src="langages.png" alt="" />
         </div>
-        <div className="bg-white rounded-xl ">
+        <div className="bg-white rounded-xl p-8 ">
           <h6 className=" text-center">Des langages essentiels</h6>
-          <p>
+          <p className="text-2xl">
             Vous apprenez trois langages essentiels : HTML, CSS et Python. Ils
             vous permettent de créer des pages web complètes, de les styliser
             avec professionnalisme et d'y ajouter de la logique pour automatiser
             ou développer des mini-programmes.
           </p>
         </div>
-        <div className="bg-white rounded-xl">
-          <p>
+        <div className="bg-white rounded-xl p-8">
+          <h1 className=" text-center">Des exercices réels</h1>
+
+          <p className=" text-2xl">
             Vous avez accès à plusieurs exercices pratiques conçus pour vous
             faire progresser pas à pas. Ces défis vous permettent d'améliorer
             votre logique, de renforcer vos compétences en programmation et de
             créer des projets concrets.
+          </p>
+        </div>
+        <div className="p-8">
+          <img className="mx-auto w-1/4" src="langages.png" alt="" />
+        </div>
+        <div className="p-8">
+          <img className="mx-auto w-2/4" src="aliens.png" alt="" />
+        </div>
+        <div className="bg-white rounded-xl p-8">
+          <h1 className=" text-center">Une progession linéaire</h1>
+          <p className="text-2xl">
+            Rejoignez une communauté actifs qui progressent chaque jour dans
+            l'apprentissage du développement web/Python. Que vous soyez débutant
+            ou déjà passionné, vous apprendrez aux côtés d'autres membres
+            motivés, partageant leurs projets, leurs questions et leurs
+            réussites pour progresser ensemble dans la programmation.
           </p>
         </div>
       </div>
