bdb2741f1363a34ff271c7ce96ef841563eae5a4
EGOUCHOLA <egoucholamalick229@gmail.com>
2025-11-05 13:42:33 +0100

    code de base

---
+++ src/app/accueil/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function SignInPage() {
+
+
+  return (
+    <div>
+
+      
+      <div className="bg-[url(/bg2.jpg)] h-[300px]">
+        <p>Hello</p>
+        <img src="logo.png" alt="" className="mx-auto"/>
+      </div>
+
+      <img src="logo.png" alt="" className="center-img"/>
+      <p className="text-3xl font-bold text-zinc-900 sm:text-6xl text-center">Bienvenue sur Gamicode, le site d’apprentissage pour maîtriser HTML, CSS et Python à ton rythme.</p>
+      </div>
+
+    
+  );
+}
