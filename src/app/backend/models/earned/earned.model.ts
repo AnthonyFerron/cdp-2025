3cf712016ac5d4156d8337cbb60baef18cc34de8
matpaaa <ma.paoly@gmail.com>
2025-11-06 19:20:53 +0100

    EarnedBusinessLogic/Crud

---
+++ src/app/backend/models/earned/earned.model.ts
@@ -0,0 +1,16 @@
+import { IdBadge, IdUser } from "../../types/custom.types"
+
+
+export type EarnedDb = {
+    id_badge: number
+    is_equiped: boolean
+    earned_at: Date
+    id_user: string
+}
+
+export type Earned = {
+    idBadge: IdBadge
+    isEquiped: boolean
+    earnedAt: Date
+    idUser: IdUser
+}
